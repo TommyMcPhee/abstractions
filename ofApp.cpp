51114303be7e927ebbaa1335b3777371cbd5cf82
Tommy McPhee <135555457+TommyMcPhee@users.noreply.github.com>
2025-10-28 14:16:26 +0000

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,16 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    cout << "Welcome to Abstractions!" << endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    //is the update function needed
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+
+}
