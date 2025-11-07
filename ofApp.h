2b984d343373e2b909b9003ea8c3acc5a7aec46a
TommyMcPhee <tommym888@gmail.com>
2025-11-06 17:15:52 -0700

    test

--- ofApp.h
+++ ofApp.h
@@ -5,6 +5,11 @@
 class ofApp : public ofBaseApp{
 
 	public:
+		ofSoundStreamSettings settings;
+		ofSoundStream stream;
+		void ofSoundStreamSetup(ofSoundStreamSettings &settings);
+		void audioIn(ofSoundBuffer &inBuffer);
+		void audioOut(ofSoundBuffer &outBuffer);
 		void setup() override;
 		void update() override;
 		void exit() override;
