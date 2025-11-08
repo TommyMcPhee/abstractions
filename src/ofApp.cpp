1734c266cd64cdd5e4557c70e42871a738c41559
TommyMcPhee <tommym888@gmail.com>
2025-11-08 01:28:11 -0700

    test

---
+++ src/ofApp.cpp
@@ -0,0 +1,63 @@
+#include "ofApp.h"
+#include "ofSoundBaseTypes.h"
+
+//--------------------------------------------------------------
+
+void ofApp::ofSoundStreamSetup(ofSoundStreamSettings &settings){
+
+}
+
+void ofApp::setup(){
+    cout << "Welcome to Abstractions!" << endl;
+    settings.setOutListener(this);
+    settings.setInListener(this);
+    settings.bufferSize = 256;
+    settings.sampleRate = 48000;
+    cout << stream.getDeviceList() << endl;
+    cout << "Enter index of input device:" << endl;
+    std::cin >> inDeviceIndex;
+    //input code
+    settings.setInDevice(stream.getDeviceList()[inDeviceIndex]);
+    cout << "Enter index of output device:" << endl;
+    std::cin >> outDeviceIndex;
+    //make dynamic to the device
+    settings.numInputChannels = inputChannels;
+    //inputcode
+    settings.setOutDevice(stream.getDeviceList()[outDeviceIndex]);
+    //make dynamic to the device
+    settings.numOutputChannels = outputChannels;
+    //set up supported sample rates and buffer sizes
+    
+    //API??
+
+    stream.setup(settings);
+}
+
+void ofApp::audioIn(ofSoundBuffer &buffer){
+    for(int a = 0; a < buffer.getNumFrames(); a++){
+        for(int b = 0; b < outputChannels; b++){
+        inputBuffer[a * inputChannels + b] = buffer[a * inputChannels + b];
+        }
+    }
+}
+
+void ofApp::audioOut(ofSoundBuffer &buffer){
+    for(int a = 0; a < buffer.getNumFrames(); a++){
+        for(int b = 0; b < outputChannels; b++){
+        sample = (inputBuffer[a * outputChannels + b] - lastSample) / 2.0;
+        buffer[a * outputChannels + b] = sample;
+        lastSample = sample;
+        //buffer[a * outputChannels + b] =  inputBuffer[a * outputChannels + b];
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    //is the update function needed
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofSoundStream.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup() override;
+		void ofSoundStreamSetup(ofSoundStreamSettings &settings);
+		void audioIn(ofSoundBuffer &buffer);
+		void audioOut(ofSoundBuffer &buffer);
+		void update() override;
+		void exit() override;
+		ofSoundStreamSettings settings;
+		unsigned int inDeviceIndex, outDeviceIndex;
+		ofSoundStream stream;
+		static const int bufferSize = 256;
+		static const int inputChannels = 2;
+		static const int outputChannels = 2;
+		float inputBuffer[bufferSize * inputChannels];
+		float sample, lastSample = 0.0;
+		//this test code will have alternate behavior if inputChannels != outputChannels
+};
