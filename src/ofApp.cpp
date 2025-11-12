e14e65864e416c57799e77db1a676a9ac95ad13d
Tommy McPhee <135555457+TommyMcPhee@users.noreply.github.com>
2025-11-12 18:17:19 +0000

    Add files via upload

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,24 +12,35 @@ void ofApp::setup(){
     settings.setOutListener(this);
     settings.setInListener(this);
     settings.bufferSize = 256;
-    settings.sampleRate = 48000;
     cout << stream.getDeviceList() << endl;
     cout << "Enter index of input device:" << endl;
     std::cin >> inDeviceIndex;
-    //input code
-    settings.setInDevice(stream.getDeviceList()[inDeviceIndex]);
+    inDevice = stream.getDeviceList()[inDeviceIndex];
+    settings.setInDevice(inDevice);
     cout << "Enter index of output device:" << endl;
     std::cin >> outDeviceIndex;
+    outDevice = stream.getDeviceList()[outDeviceIndex];
     //make dynamic to the device
     settings.numInputChannels = inputChannels;
     //inputcode
     settings.setOutDevice(stream.getDeviceList()[outDeviceIndex]);
+    for(int a = 0; a < inDevice.sampleRates.size(); a++){
+    cout << inDevice.sampleRates[a] << endl;
+    }
+    
     //make dynamic to the device
     settings.numOutputChannels = outputChannels;
     //set up supported sample rates and buffer sizes
-    
+    //FIX
+    cout << "Press any key for advanced settings, ENTER to begin the piece." << endl;
+    if (std::cin.get() == '\n'){
+        cout << "Good job.\n";
+    }
+    else{
+    cout << "I meant ONLY the ENTER key... Oh well.\n";
+    }
     //API??
-
+    settings.sampleRate = 48000;
     stream.setup(settings);
 }
 
@@ -42,7 +53,9 @@ void ofApp::audioIn(ofSoundBuffer &buffer){
 }
 
 void ofApp::audioOut(ofSoundBuffer &buffer){
-    cout << phase << endl;
+    //cout << phase << endl;
+    cout << testInt << endl;
+        
     for(int a = 0; a < buffer.getNumFrames(); a++){
         for(int b = 0; b < outputChannels; b++){
         phase += pow(abs(inputBuffer[a * outputChannels + b] - lastSample) * 0.5, 4.0) * M_PI;
@@ -50,6 +63,7 @@ void ofApp::audioOut(ofSoundBuffer &buffer){
         sample = sin(phase);
         buffer[a * outputChannels + b] = sample;
         lastSample = sample;
+        testInt = 0;
         //buffer[a * outputChannels + b] =  inputBuffer[a * outputChannels + b];
         }
     }
@@ -58,6 +72,7 @@ void ofApp::audioOut(ofSoundBuffer &buffer){
 //--------------------------------------------------------------
 void ofApp::update(){
     //is the update function needed
+    testInt++;
 }
 
 //--------------------------------------------------------------
